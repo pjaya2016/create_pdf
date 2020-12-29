b4329a051355de83cf1a44fd295484a0f37d41f6
pjaya2016 <pjayamayooragan2016@gmail.com>
2020-12-29 09:22:22 +0000

    Added table

--- src/main/java/com/importexport/demo/PDFUtil.java
+++ src/main/java/com/importexport/demo/PDFUtil.java
@@ -2,6 +2,8 @@ package com.importexport.demo;
 
 
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
@@ -53,7 +55,86 @@ public class PDFUtil {
                 case "<key-paragraph>":
                     appendParagraph(group);
                     break;
+                case "<key-table>":
+                    appendTable(group);
+                    break;
             }
+        }
+    }
+
+    private void appendTable(String group) {
+        try {
+           /* Pattern pattern = Pattern.compile("(?!=<key-paragraph>)[A-z0-9!\"£$%^&*(){}\\[\\]@;:?/`¬,()~|#\\s+=]+(?=<\\/key-paragraph>)");
+            String[] arr = group.split("(?!<\\/key-table>)[~\\s]+(?=<key\\-table>)");
+            PdfPTable table = new PdfPTable(1); // 3 columns.
+            table.setWidthPercentage(100); //Width 100%
+            table.setSpacingBefore(10f); //Space before table
+            table.setSpacingAfter(10f); //Space after table
+
+            //Set Column widths
+            float[] columnWidths = {1f, 1f, 1f};
+            table.setWidths(columnWidths);
+            for (int i = 0; i < arr.length; i++) {
+                Paragraph paragraph = new Paragraph();
+                try {
+                    Matcher matcher = pattern.matcher(arr[i]);
+                    while (matcher.find()) {
+
+                        String val = matcher.group();
+                        paragraph.add(val);
+                        PdfPCell cell1 = new PdfPCell(new Paragraph(paragraph));
+                        cell1.setBorderColor(BaseColor.BLUE);
+                        cell1.setPaddingLeft(10);
+                        cell1.setHorizontalAlignment(Element.ALIGN_CENTER);
+                        cell1.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                        table.addCell(cell1);
+                    }
+                    doc.add(table);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }*
+
+            */
+
+            PdfPTable table = new PdfPTable(3); // 3 columns.
+            table.setWidthPercentage(100); //Width 100%
+            table.setSpacingBefore(10f); //Space before table
+            table.setSpacingAfter(10f); //Space after table
+
+            //Set Column widths
+            float[] columnWidths = {1f, 1f, 1f};
+            table.setWidths(columnWidths);
+
+            PdfPCell cell1 = new PdfPCell(new Paragraph("1"));
+            cell1.setBorderColor(BaseColor.BLUE);
+            cell1.setPaddingLeft(10);
+            cell1.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell1.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+            PdfPCell cell2 = new PdfPCell(new Paragraph("2"));
+            cell2.setBorderColor(BaseColor.GREEN);
+            cell2.setPaddingLeft(10);
+            cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell2.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+            PdfPCell cell3 = new PdfPCell(new Paragraph("3"));
+            cell3.setBorderColor(BaseColor.RED);
+            cell3.setPaddingLeft(10);
+            cell3.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell3.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+            //To avoid having the cell border and the content overlap, if you are having thick cell borders
+            //cell1.setUserBorderPadding(true);
+            //cell2.setUserBorderPadding(true);
+            //cell3.setUserBorderPadding(true);
+
+            table.addCell(cell1);
+            table.addCell(cell2);
+            table.addCell(cell3);
+
+            doc.add(table);
+            } catch (DocumentException documentException) {
+            documentException.printStackTrace();
         }
     }
 
